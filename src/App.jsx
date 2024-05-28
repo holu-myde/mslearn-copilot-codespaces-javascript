571c62e583f052867a5068064cf0955772c6bc97
Olumide Ebigbola <42293717+holu-myde@users.noreply.github.com>
2024-05-28 08:37:44 +0000

    updates to javascript project

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Olumide Ebigbola",
+  title: "Product Manager & Cloud Engineer",
+  email: "olumideebigbola@gmail.com",
+  gitHub: "holu_myde",
+  instagram: "holu_myde",
+  linkedIn: "https://www.linkedin.com/in/olumide-ebigbola-ab1474a3/",
+  medium: "Olumide Ebigbola",
+  twitter: "holu_myde",
+  youTube: "Olumide Ebigbola",
 };
 
 const primaryColor = "#4E567E";
